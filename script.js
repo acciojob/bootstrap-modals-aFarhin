5b1713742df1ef8b99a462ac5c6305579702a6fa
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-02-28 18:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 //your code here
-<script>
-  $(document).ready(function() {
-    $('#accio-modal').modal('hide'); // hide the Modal by default
-    $('#open-modal').click(function() { // show the Modal when the button is clicked
-      $('#accio-modal').modal('show');
-    });
-  });
-</script>
-
-
